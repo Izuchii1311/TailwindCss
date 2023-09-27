e8d501cf75e7df26bc3c491f9e1a4c90e154d474
Izuchii1311 <luthfiramadhan.lr55@gmail.com>
2023-09-27 10:16:03 +0700

    Functions & Directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./public/**/*.{html,js}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+      extend: {
+          colors: {
+              clifford: '#da373d',
+          },
+          spacing: {
+              13: '3.25rem',
+          },
+          fontFamily: {
+              inter: ['Inter'],
+          },
+          colors: {
+              izu: '#bada55',
+              kopi: '#c0ffee',
+          },
+          animation: {
+              'spin-slow': 'spin 3s linear infinite',
+              'goyang': 'goyang 1s ease-in-out infinite',
+          },
+          // keyframe untuk animasi sendiri
+          keyframes: {
+              goyang: {
+                  '0%, 100%' : {transform: 'rotate(-3deg)'},
+                  '50%': {transform: 'rotate(3deg)'}
+              }
+          }
+      },
   },
   plugins: [],
 }
